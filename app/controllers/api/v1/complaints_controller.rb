3cc99805a8eb9eb89b634c6242f34c6d58b8e569
Andrew Watkins <andrew.watkinz@gmail.com>
2014-10-07 13:03:31 -0600

    doners

--- app/controllers/api/v1/complaints_controller.rb
+++ app/controllers/api/v1/complaints_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ComplaintsController<ApplicationController
   respond_to :json
 
   def index
-    @complaints = Complaint.limit(10000)
+    @complaints = Complaint.all
     respond_with @complaints, each_serializer: Api::V1::ComplaintSerializer
   end
 end
