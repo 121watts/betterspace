47a6390cc9967cef36ad350438010a7a5a4f7eaa
Andrew Watkins <andrew.watkinz@gmail.com>
2014-10-08 17:40:07 -0600

    adds sexier flash messages

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -1,6 +1,6 @@
 class ApiKeysController < ApplicationController
 
-  before_filter :allow 
+  before_filter :allow
 
   def index
     @api_keys = current_user.api_keys
@@ -9,12 +9,12 @@ class ApiKeysController < ApplicationController
   def create
     current_user.api_keys.create(token: SecureRandom.hex)
 
-    redirect_to api_keys_path, notice: "You API Key has been created."
+    redirect_to api_keys_path, notice: "Your API Key has been created."
   end
 
   def destroy
     current_user.api_keys.find(params[:id]).destroy
-
+    
     redirect_to api_keys_path, notice: "Your API Key has been deleted."
   end
 end
